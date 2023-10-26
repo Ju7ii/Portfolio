23150157012fdbbce0106140502c6fc7a00292c5
Your Name <you@example.com>
2023-10-26 15:45:15 +0200

    js functions check navbar

--- js/navigation.js
+++ js/navigation.js
@@ -1,27 +1,9 @@
 //* GLOBAL VARIABLES *//
-
-const navigationBar = document.querySelector("navigation");
+const navigationBar = document.querySelector(".navigation");
 const navigationButton = document.querySelector(".menu-toggle-button");
 const menu = document.getElementById("menu");
 const menuItem = document.querySelectorAll(".menu-item");
 const html = document.documentElement;
-
-
-
-// = EVENT LISTENERS = //
-
-//  PAGE IS SCROLLED CHECK  //
-
-window.onscroll = () => {
-    //window.scrollY > 100 ? addActiveNavbar() : removeActiveNavbar();
-    if (window.scrollY > 100) {
-        addActiveNavbar();
-    }
-    else {
-        removeActiveNavbar();
-    }
-};
-
 
 //$ FUNCTIONS $//
 
@@ -30,7 +12,7 @@ function openMenu() {
     html.style.overflowY = "hidden"; //* disable scrollbar
     navigationButton.setAttribute("aria-expanded", "true"); //* set aria state -> true
     menu.setAttribute("aria-expanded", "true"); //* set aria state -> true
-    addActiveNavbar();
+    checkIfScrolled();
 }
 
 //* CLOSE MENU *//
@@ -38,37 +20,52 @@ function closeMenu() {
     html.style.overflowY = ""; //* enable scrollbar
     navigationButton.setAttribute("aria-expanded", "false"); //* set aria state -> false
     menu.setAttribute("aria-expanded", "false"); //* set aria state -> false
-    window.scrollY > 100 ? null : removeActiveNavbar(); //* only remove the navbar when on top of site
+    checkIfScrolled();
 }
 
 //* ADD ACTIVE NAVBAR CLASS *//
 function addActiveNavbar() {
-    navigationBar.classList.add("active-navbar");
+    if (!navigationBar.classList.contains('active-navbar')) {
+        navigationBar.classList.add('active-navbar');
+    }
 }
 
-//* REMOVE ACTIVE NAVBAR CLASS *//
+//* REMOVE ACTIVE NAVBAR CLASS *// 
 function removeActiveNavbar() {
-    navigationBar.classList.remove("active-navbar");
+    if (navigationBar.classList.contains('active-navbar')) {
+        navigationBar.classList.remove('active-navbar');
+    }
 }
 
-//* CHECK IF NAVIGATION IS ALREADY OPEN
+//* CHECK IF NAVIGATION IS ALREADY OPEN 
 function isNavigationOpen() {
     const menuIsOpen = navigationButton.getAttribute("aria-expanded");
     menuIsOpen == "false" ? openMenu() : closeMenu();
 }
 
+//* HANDLE SCROLL EVENT
+function checkIfScrolled() {
+    window.scrollY > 100 ? addActiveNavbar() : removeActiveNavbar();
+}
+
 //$ END FUNCTIONS $//
+
 //!-----------------------------------------------------------------------------------------------------------------------------------------------!//
+
 //$ EVENTLISTENERS $//
 
-//* CHECK IF NAVIGATION IS OPEN -> THEN OPEN OR CLOSE IT
+//* CHECK IF NAVIGATION IS OPEN -> THEN OPEN OR CLOSE IT 
+
 navigationButton.addEventListener("click", isNavigationOpen);
 
-//* CLOSE MENU *//
+//* CLOSE MENU *// 
+
 menuItem.forEach(menuItem => {
     menuItem.addEventListener("click", () => {
         setTimeout(closeMenu, 200);
     })
 });
+
+window.addEventListener('scroll', checkIfScrolled);
 
 //$ EVENTLISTENERS $//
