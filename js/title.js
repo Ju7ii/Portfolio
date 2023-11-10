03b9f8f8651b488dfe7f72deb20522492bd2683f
Ju7ii <julian.rok@gmx.de>
2023-11-10 23:46:55 +0100

    This PR introduces a new feature that dynamically changes the website title when the user is not focused on the tab. The title cycles through the characters of a predefined string, creating a marquee-like effect. When the user refocuses on the tab, the title reverts back to its original state.

--- js/title.js
+++ js/title.js
@@ -1,22 +1,23 @@
-var character = 2;
-var newTitleText = " Innovation in Pixels: Julian's Portfolio👨‍💻";
-var currentTitle = document.title;
+let character = 2;
+let newTitleText = " Innovation in Pixels: Julian's Portfolio👨‍💻";
+let currentTitle = document.title;
+
 function counter() {
     counter.timer = setInterval(function () {
         document.title = newTitleText.substring(character, newTitleText.length) + newTitleText.substring(0, character);
         character++;
-        if (character > message.length) {
+        if (character > newTitleText.length) {
             character = 0;
         }
     }, 500);
 }
+
 function stop() {
     clearInterval(counter.timer);
 }
-window.onblur = function () {
-    counter();
-};
-window.onfocus = function () {
+
+window.addEventListener('blur', counter);
+window.addEventListener('focus', function() {
     stop();
     document.title = currentTitle;
-};
+});
